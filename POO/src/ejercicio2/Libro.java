2da5076626d96605b31bdfb7818c074f7cedcbdc
igarcia <igarcar@iesnervion.es>
2023-03-14 11:06:16 +0100

    14/03/2023

--- POO/src/ejercicio2/Libro.java
+++ POO/src/ejercicio2/Libro.java
@@ -2,35 +2,70 @@ package ejercicio2;
 
 public class Libro {
 
+	/**
+	 * Titulo del libro
+	 */
 	String titulo;
+
+	/**
+	 * Autor del libro
+	 */
 	String autor;
+
+	/**
+	 * Cantidad de ejemplares
+	 */
 	int numeroEjemplares;
+
+	/**
+	 * Ejemplares prestados
+	 */
 	int ejemplaresPrestados;
 
+	/**
+	 * Contructor por defecto
+	 */
 	public Libro() {
 	}
 
+	/**
+	 * Constructor con parametros
+	 * 
+	 * @param titulo              Titulo del libro
+	 * @param autor               Autor del libro
+	 * @param numeroEjemplares    Cantidad de ejemplares
+	 * @param ejemplaresPrestados Ejemplares prestados
+	 */
 	public Libro(String titulo, String autor, int numeroEjemplares, int ejemplaresPrestados) {
 		this.titulo = "El quijote";
 		this.autor = "Migule de Cervantes";
-		this.numeroEjemplares = 27000000;
-		this.ejemplaresPrestados = 4000000;
+		this.numeroEjemplares = 270;
+		this.ejemplaresPrestados = 4;
 	}
 
+	/**
+	 * Metodo para prestar un ejemplar
+	 * 
+	 * @return Si se ha podido prestar o no
+	 */
 	boolean prestamo() {
 
 		boolean prestado = true;
 
-		if (numeroEjemplares <= 0) {
+		if (ejemplaresPrestados >= numeroEjemplares) {
 			prestado = false;
 		} else {
 			ejemplaresPrestados++;
-			numeroEjemplares--;
 		}
 
 		return prestado;
 	}
 
+	/**
+	 * Metodo para devolver un libro prestado
+	 * 
+	 * @return Si se ha podido devolver
+	 */
 	boolean devolución() {
 
 		boolean devuelto = true;
--- POO/src/ejercicio2/Main.java
+++ POO/src/ejercicio2/Main.java
@@ -1,15 +1,48 @@
 package ejercicio2;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		// Declaración de variables
-		int opc=0;
+		int opc = 0;
+
+		Libro libro = new Libro();
+
+		Scanner sc = new Scanner(System.in);
+
+		// Pedimos que quiere hacer al usuario
+		System.out.println("1. Prestamo");
+		System.out.println("2. Devolución");
+		System.out.println();
+		System.out.print("¿Que quieres hacer?: ");
+		opc = sc.nextInt();
+
+		switch (opc) {
+		case 1 -> {
+			if(libro.prestamo()) {
+				System.out.println("Prestamo exitoso");
+			} else {
+				System.out.println("Lo sentimos pero no hay ejemplares actualmente");
+			}
+		}
+
+		case 2 -> {
+			if(libro.devolución()) {
+				System.out.println("Devolución exitosa");
+			} else {
+				System.out.println("Lo sentimos, no podemos recoger un libro que no se ha prestado");
+			}
+		}
+
+		default -> {
+			System.err.println("Opción no valida");
+		}
+		}
 		
-		Libro libro= new Libro();
-		
-		
+		sc.close();
 
 	}
 
