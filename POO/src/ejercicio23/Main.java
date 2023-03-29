3aa4d9ce5a261ce43df8bbb5283a61de07a36edc
igarcia <igarcar@iesnervion.es>
2023-03-29 14:27:55 +0200

    29/03/2023

--- POO/src/ejercicio23/Main.java
+++ POO/src/ejercicio23/Main.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		// Creamos el objeto
 		Fecha f1 = new Fecha();
-		
+
 		// Abrimos el escaner
 		Scanner sc = new Scanner(System.in);
-		
+
 		// Pedimos la fecha al usuario
 		System.out.println("¿Que dia es?");
 		System.out.print("Dia: ");
@@ -20,20 +20,30 @@ public class Main {
 		f1.setMes(sc.nextInt());
 		System.out.print("Año: ");
 		f1.setAño(sc.nextInt());
-		
-		if(!f1.fechaCorrecta()) {
+
+		// Comprobamos si la fecha es correcta
+		if (!f1.fechaCorrecta()) {
 			System.err.println("La fecha no es correcta");
 		}
-		
+
+		// Pasamos al siguiente dia
 		f1.diaSiguiente();
-		
-		if(f1.esBisiesto()) {
+
+		// Comprobamos si el año es bisiesto
+		if (f1.esBisiesto()) {
 			System.out.println("El año es bisisesto");
 		}
-		
+
+		// Mostramos la fecha
 		System.out.println(f1.toString());
-		
+
+		// Cambiamos el mes
 		f1.setMes(sc.nextInt());
+
+		// Mostramos la fecha
+		System.out.println(f1.toString());
+
+		sc.close();
 
 	}
 
